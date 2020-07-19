ab0804be2a69085fc19a9ccf50c25f90c80d46e7
Abhilash Sulibela <abhilash1869@gmail.com>
2020-07-19 17:36:34 -0500

    feat(module-app): add search feature to filter users by name

--- portal/src/app/components/users/users.component.ts
+++ portal/src/app/components/users/users.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../services/user.service";
 import { User } from "../../models/user"
-import { Employment } from "../../models/employment"
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -12,11 +9,16 @@ import { Observable } from 'rxjs';
 })
 export class UsersComponent implements OnInit {
 
+  unfiltered_users: User[]
   users: User[]
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(users => this.users = users);
+    this.userService.getUsers().subscribe(users => this.users = this.unfiltered_users = users);
   };
+
+  filter(term: string) {
+    this.users = this.unfiltered_users.filter(user => user.name.startsWith(term));
+  }
 
 }
