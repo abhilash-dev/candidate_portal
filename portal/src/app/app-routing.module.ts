9daab1bc729ba9889b875a82a3775f12c3db831c
Abhilash Sulibela <abhilash1869@gmail.com>
2020-07-19 00:26:20 -0500

    feat(module-app): import components & set up routing for the app

--- portal/src/app/app-routing.module.ts
+++ portal/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AddUserComponent } from "./components/add-user/add-user.component"
+import { DashboardComponent } from "./components/dashboard/dashboard.component"
+import { LoginComponent } from "./components/login/login.component"
+import { NavbarComponent } from "./components/navbar/navbar.component"
+import { NotFoundComponent } from "./components/not-found/not-found.component"
+import { RegisterComponent } from "./components/register/register.component"
+import { SidebarComponent } from "./components/sidebar/sidebar.component"
+import { UsersComponent } from "./components/users/users.component"
+import { UserDetailsComponent } from "./components/user-details/user-details.component"
+import { EditUserComponent } from "./components/edit-user/edit-user.component"
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "", component: DashboardComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "user/add", component: AddUserComponent },
+  { path: "user/edit/:id", component: EditUserComponent },
+  { path: "user/:id", component: UserDetailsComponent },
+  { path: "**", component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portal/src/app/app.module.ts
+++ portal/src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from "../environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore"
+import { AngularFireAuthModule } from "@angular/fire/auth"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UsersComponent } from './components/users/users.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+
+import { UserService } from "./services/user.service"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    UsersComponent,
+    SidebarComponent,
+    AddUserComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    UserDetailsComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'candidateportal'),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
